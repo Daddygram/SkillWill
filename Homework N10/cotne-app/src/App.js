69b24fd3ef733edc0423186cdb8e47e3a5e0c98e
Tsotne Skhvitaridze <89193876+Daddygram@users.noreply.github.com>
2023-06-24 21:01:19 +0400

    Add files via upload

---
+++ Homework N10/cotne-app/src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+
+    <header className="head">
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+      
+  );
+}
+
+export default App;
